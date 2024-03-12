10159e9d9587d94ae66f8de706e220fb1743844a
JayantRajpoot <jayantrajput1004@gmail.com>
2024-03-12 20:35:49 +0530

    4th Commit

---
+++ random_head_and_tails.py
@@ -0,0 +1,6 @@
+import random as rm
+a = rm.randint(1,2)
+if (a == 1):
+    print("Tails")
+if (a == 2):
+    print("Heads")
---
+++ random_list_select.py
@@ -0,0 +1,5 @@
+
+names = ["Jagga", "Rahul", "VHdj", "fdsf"]
+import random
+a = random.randint(0,len(names)-1)
+print(f"{names[a]} is going to buy the meal today!")
