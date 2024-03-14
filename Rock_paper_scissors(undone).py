56b13f0de35d513c6729c393ba3101e54155ea3c
JayantRajpoot <jayantrajput1004@gmail.com>
2024-03-14 20:57:46 +0530

    not complete rock_paper

---
+++ Rock_paper_scissors(undone).py
@@ -0,0 +1,54 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random as rm
+pcdraw = rm.randint(1,3)
+print("Hey Let's play Rock for 1 Paper for 2 and Scissors for 3")
+ans = input('What you wanna draw Rock, paper, scissors.\n').lower()
+print("Your Draw")
+if(ans == "1"):
+    print(rock)
+    ans = 1
+if (ans == "2"):
+    print (paper)
+    ans = 2
+if (ans == "3"):
+    print(scissors)
+    ans = 3
+print("Computer Draw:")
+if(pcdraw == 1):
+    print(rock)
+
+if (pcdraw == 2):
+    print (paper)
+if (pcdraw == 3):
+    print(scissors)
+
+if (ans == pcdraw):
+    print("It's Draw Start again")
+if (ans < pcdraw or ):
+    
+    
